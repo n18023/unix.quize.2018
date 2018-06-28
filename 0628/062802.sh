ce76ece0537f81061aba7b46475bbfec2cf497d9
n18023 <n18023@std.it-college.ac.jp>
2018-06-28 03:51:36 +0000

    0628test

--- 0628/062802.sh
+++ 0628/062802.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-du -b ~/kadai | sort -n | tac | head -n 5
 
 if [ "$#" -eq 0 ]; then
 	echo Usage: 062802.sh PATH
 	exit 1
 fi
 
-echo $1
+find $# -type f -name | du -b $# | sort -n | tac | head -n 5
